722c5a5412a56b76897e1f98565197a89d3bdaba
haozengrun <haozengrun@duiba.com.cn>
2022-09-24 21:38:35 +0800

    -

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,25 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+// import express, { Application, Request, Response, NextFunction } from "express";
 
-var http = require('http');
+// var http = require('http');
 
-const app: Application = express();
+// const app: Application = express();
 
-// app.get("/", (req: Request, res: Response, next: NextFunction) => {
-//   res.send("Hello World!");
+// // app.get("/", (req: Request, res: Response, next: NextFunction) => {
+// //   res.send("Hello World!");
+// // });
+
+// var server = http.createServer(app);
+
+// const io = require('socket.io')(server)
+
+// io.on('connection', (socket: any) => {
+//   console.log('有一个客户端连接了服务器：', socket)
+// })
+
+// server.listen(7071, function () {
+//   console.log("Example app listening on port 7071!");
 // });
 
-var server = http.createServer(app);
-
-const io = require('socket.io')(server)
-
-io.on('connection', (socket: any) => {
-  console.log('有一个客户端连接了服务器：', socket)
-})
-
-server.listen(7071, function () {
-  console.log("Example app listening on port 7071!");
-});
+const app = require('http').createServer() // 本地开发服务器
+const io = require('socket.io')(app)
+app.listen(7071)
--- build/app.js
+++ build/app.js
@@ -1,19 +1,18 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-var http = require('http');
-const app = (0, express_1.default)();
-// app.get("/", (req: Request, res: Response, next: NextFunction) => {
-//   res.send("Hello World!");
+// import express, { Application, Request, Response, NextFunction } from "express";
+// var http = require('http');
+// const app: Application = express();
+// // app.get("/", (req: Request, res: Response, next: NextFunction) => {
+// //   res.send("Hello World!");
+// // });
+// var server = http.createServer(app);
+// const io = require('socket.io')(server)
+// io.on('connection', (socket: any) => {
+//   console.log('有一个客户端连接了服务器：', socket)
+// })
+// server.listen(7071, function () {
+//   console.log("Example app listening on port 7071!");
 // });
-var server = http.createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-    console.log('有一个客户端连接了服务器：', socket);
-});
-server.listen(7071, function () {
-    console.log("Example app listening on port 7071!");
-});
+const app = require('http').createServer(); // 本地开发服务器
+const io = require('socket.io')(app);
+app.listen(7071);
