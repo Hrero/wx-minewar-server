2a5a2c978a63da56b8209ca2cd533f31dfb4b524
haozengrun <haozengrun@duiba.com.cn>
2022-09-24 19:07:24 +0800

    -

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,19 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+
 const app: Application = express();
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World!");
 });
+
+const io = require('socket.io')(app)
+
+// 有客户端连接服务器
+io.on('connection', (socket: any) => {
+  console.log('有一个客户端连接了服务器：', socket)
+})
+
 app.listen(7071, function () {
   console.log("Example app listening on port 7071!");
 });
+
--- build/app.js
+++ build/app.js
@@ -8,6 +8,11 @@ const app = (0, express_1.default)();
 app.get("/", (req, res, next) => {
     res.send("Hello World!");
 });
+const io = require('socket.io')(app);
+// 有客户端连接服务器
+io.on('connection', (socket) => {
+    console.log('有一个客户端连接了服务器：', socket);
+});
 app.listen(7071, function () {
     console.log("Example app listening on port 7071!");
 });
