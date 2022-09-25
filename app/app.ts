80ac859605a4fb6d325743b718be8e5ec75b9fb8
haozengrun <haozengrun@duiba.com.cn>
2022-09-25 17:16:59 +0800

    -

--- app/app.ts
+++ app/app.ts
@@ -20,7 +20,51 @@
 //   console.log("Example app listening on port 7071!");
 // });
 
-const app = require('http').createServer(function serveIndex (req: any, res: any) {
+// const app = require('http').createServer(function serveIndex (req: any, res: any) {
+//   res.write(`
+//     <html>
+//     <head>
+//     <title>微信小游戏</title>
+//     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+//     </head>
+//     <body>
+//     <header>
+//     <h1>《微信小游戏开发》</h1>
+//     </header>
+//     <ul>
+//       <li><a href="https://www.haozengrun.com">主页跳转</a></li>
+//       <li>游戏专用域名</li>
+//     </ul>
+
+//     <footer>ELEVEN</footer>
+//     </body>
+//     </html>
+//   `)
+//   res.end()
+// })
+
+// // 创建 socket.io 服务
+// const io = require('socket.io')(app)
+
+// // 监听 https 端口
+// app.listen(7071)
+
+// // 有客户端连接服务器
+// io.on('connection', function (socket: any) {
+//   console.log('有一个客户端连接了服务器：', socket)
+//   // socket 代表该客户端的 socket.io 连接
+// })
+
+
+
+
+const http = require('http');
+const WebS = require('ws');
+const fs = require('fs')
+const buffer = require('buffer');
+
+//创建服务器
+const server = require("http").createServer(function serveIndex (req: any, res: any) {
   res.write(`
     <html>
     <head>
@@ -43,14 +87,21 @@ const app = require('http').createServer(function serveIndex (req: any, res: any
   res.end()
 })
 
-// 创建 socket.io 服务
-const io = require('socket.io')(app)
+//使用websocket
+const ws = new WebS.Server({server});
+ws.on("connection", (ws: { send: (arg0: string) => void; on: (arg0: string, arg1: (e: any) => void) => void; }) => {
+    console.log("已连接客户端");
+    //发送消息给客户端
+    ws.send("hello client!")
+    //监听客户端发来的消息
+    ws.on("message", e => {
+        //接收到的是一个buffer类型的数据，如<Buffer 68 65 6c 6c 6f 20 73 65 72 76 65 72>
+        console.log(e)
+        console.log(e.toString())   //将buffer转为字符串
+    })
+})
 
-// 监听 https 端口
-app.listen(7071)
-
-// 有客户端连接服务器
-io.on('connection', function (socket: any) {
-  console.log('有一个客户端连接了服务器：', socket)
-  // socket 代表该客户端的 socket.io 连接
+//添加监听
+server.listen(7071, () => {
+    console.log("localhost:7071")
 })
