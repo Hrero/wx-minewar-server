8c993650bdae4fe65b42e4fd3bf85d4a0a935fc2
haozengrun <haozengrun@duiba.com.cn>
2022-09-25 15:01:39 +0800

    -

--- app/app.ts
+++ app/app.ts
@@ -1,108 +1,125 @@
-import express from "express";
-import { createServer } from "http";
 import { Server } from "socket.io";
-import session from "express-session";
 
-const port = process.env.PORT || 7071;
-
-const app = express();
-const httpServer = createServer(app);
-
-const sessionMiddleware = session({
-  secret: "changeit",
-  resave: true,
-  saveUninitialized: true,
-});
-
-app.use(sessionMiddleware);
-
-app.get("/", function serveIndex (req: any, res: any) {
-    res.write(`
-      <html>
-      <head>
-      <title>微信小游戏</title>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      </head>
-      <body>
-      <header>
-      <h1>《微信小游戏开发》</h1>
-      </header>
-      <ul>
-        <li><a href="https://www.haozengrun.com">主页跳转</a></li>
-        <li>游戏专用域名</li>
-      </ul>
-  
-      <footer>ELEVEN</footer>
-      </body>
-      </html>
-    `)
-    res.end()
-});
-
-app.post("/incr", (req, res) => {
-  const session: any = req.session;
-  session.count = (session?.count || 0) + 1;
-  res.status(200).end("" + session?.count);
-});
-
-app.post("/logout", (req, res) => {
-  const sessionId = req.session.id;
-  req.session.destroy(() => {
-    io.to(sessionId).disconnectSockets();
-    res.status(204).end();
-  });
-});
-
-const io = new Server(httpServer, {
-  allowRequest: (req: any, callback) => {
-    const fakeRes = {
-      getHeader() {
-        return [];
-      },
-      setHeader(key: any, values: any) {
-        req.cookieHolder = values[0];
-      },
-      writeHead() {},
-    };
-    // @ts-ignore
-    sessionMiddleware(req, fakeRes, () => {
-      if (req.session) {
-        fakeRes.writeHead();
-        req.session.save();
-      }
-      callback(null, true);
-    });
-  },
-});
-
-io.engine.on("initial_headers", (headers: { [x: string]: any; }, req: { cookieHolder: any; }) => {
-  if (req.cookieHolder) {
-    headers["set-cookie"] = req.cookieHolder;
-    delete req.cookieHolder;
-  }
-});
-
+const io = new Server(7071);
+console.log(io);
+console.log(7071);
 io.on("connect", (socket) => {
-  const req: any = socket.request;
+    console.log(`connect ${socket.id}`);
 
-  socket.join(req.session.id);
-
-  socket.on("incr", (cb) => {
-    req.session.reload((err: any) => {
-      if (err) {
-        return socket.disconnect();
-      }
-      req.session.count = (req.session.count || 0) + 1;
-      req.session.save(() => {
-        cb(req.session.count);
-      });
+    socket.on("ping", (cb) => {
+        console.log("ping");
+        cb();
     });
-  });
-});
 
-httpServer.listen(port, () => {
-  console.log(`application is running at: http://localhost:${port}`);
+    socket.on("disconnect", () => {
+        console.log(`disconnect ${socket.id}`);
+    });
 });
+// import express from "express";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// import session from "express-session";
+
+// const port = process.env.PORT || 7071;
+
+// const app = express();
+// const httpServer = createServer(app);
+
+// const sessionMiddleware = session({
+//   secret: "changeit",
+//   resave: true,
+//   saveUninitialized: true,
+// });
+
+// app.use(sessionMiddleware);
+
+// app.get("/", function serveIndex (req: any, res: any) {
+//     res.write(`
+//       <html>
+//       <head>
+//       <title>微信小游戏</title>
+//       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+//       </head>
+//       <body>
+//       <header>
+//       <h1>《微信小游戏开发》</h1>
+//       </header>
+//       <ul>
+//         <li><a href="https://www.haozengrun.com">主页跳转</a></li>
+//         <li>游戏专用域名</li>
+//       </ul>
+  
+//       <footer>ELEVEN</footer>
+//       </body>
+//       </html>
+//     `)
+//     res.end()
+// });
+
+// app.post("/incr", (req, res) => {
+//   const session: any = req.session;
+//   session.count = (session?.count || 0) + 1;
+//   res.status(200).end("" + session?.count);
+// });
+
+// app.post("/logout", (req, res) => {
+//   const sessionId = req.session.id;
+//   req.session.destroy(() => {
+//     io.to(sessionId).disconnectSockets();
+//     res.status(204).end();
+//   });
+// });
+
+// const io = new Server(httpServer, {
+//   allowRequest: (req: any, callback) => {
+//     const fakeRes = {
+//       getHeader() {
+//         return [];
+//       },
+//       setHeader(key: any, values: any) {
+//         req.cookieHolder = values[0];
+//       },
+//       writeHead() {},
+//     };
+//     // @ts-ignore
+//     sessionMiddleware(req, fakeRes, () => {
+//       if (req.session) {
+//         fakeRes.writeHead();
+//         req.session.save();
+//       }
+//       callback(null, true);
+//     });
+//   },
+// });
+
+// io.engine.on("initial_headers", (headers: { [x: string]: any; }, req: { cookieHolder: any; }) => {
+//   if (req.cookieHolder) {
+//     headers["set-cookie"] = req.cookieHolder;
+//     delete req.cookieHolder;
+//   }
+// });
+
+// io.on("connect", (socket) => {
+//   const req: any = socket.request;
+
+//   socket.join(req.session.id);
+
+//   socket.on("incr", (cb) => {
+//     req.session.reload((err: any) => {
+//       if (err) {
+//         return socket.disconnect();
+//       }
+//       req.session.count = (req.session.count || 0) + 1;
+//       req.session.save(() => {
+//         cb(req.session.count);
+//       });
+//     });
+//   });
+// });
+
+// httpServer.listen(port, () => {
+//   console.log(`application is running at: http://localhost:${port}`);
+// });
 
 // // import express, { Application, Request, Response, NextFunction } from "express";
 
--- build/app.js
+++ build/app.js
@@ -1,100 +1,110 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const http_1 = require("http");
 const socket_io_1 = require("socket.io");
-const express_session_1 = __importDefault(require("express-session"));
-const port = process.env.PORT || 7071;
-const app = (0, express_1.default)();
-const httpServer = (0, http_1.createServer)(app);
-const sessionMiddleware = (0, express_session_1.default)({
-    secret: "changeit",
-    resave: true,
-    saveUninitialized: true,
-});
-app.use(sessionMiddleware);
-app.get("/", function serveIndex(req, res) {
-    res.write(`
-      <html>
-      <head>
-      <title>微信小游戏</title>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      </head>
-      <body>
-      <header>
-      <h1>《微信小游戏开发》</h1>
-      </header>
-      <ul>
-        <li><a href="https://www.haozengrun.com">主页跳转</a></li>
-        <li>游戏专用域名</li>
-      </ul>
-  
-      <footer>ELEVEN</footer>
-      </body>
-      </html>
-    `);
-    res.end();
-});
-app.post("/incr", (req, res) => {
-    const session = req.session;
-    session.count = ((session === null || session === void 0 ? void 0 : session.count) || 0) + 1;
-    res.status(200).end("" + (session === null || session === void 0 ? void 0 : session.count));
-});
-app.post("/logout", (req, res) => {
-    const sessionId = req.session.id;
-    req.session.destroy(() => {
-        io.to(sessionId).disconnectSockets();
-        res.status(204).end();
-    });
-});
-const io = new socket_io_1.Server(httpServer, {
-    allowRequest: (req, callback) => {
-        const fakeRes = {
-            getHeader() {
-                return [];
-            },
-            setHeader(key, values) {
-                req.cookieHolder = values[0];
-            },
-            writeHead() { },
-        };
-        // @ts-ignore
-        sessionMiddleware(req, fakeRes, () => {
-            if (req.session) {
-                fakeRes.writeHead();
-                req.session.save();
-            }
-            callback(null, true);
-        });
-    },
-});
-io.engine.on("initial_headers", (headers, req) => {
-    if (req.cookieHolder) {
-        headers["set-cookie"] = req.cookieHolder;
-        delete req.cookieHolder;
-    }
-});
+const io = new socket_io_1.Server(7071);
+console.log(io);
+console.log(7071);
 io.on("connect", (socket) => {
-    const req = socket.request;
-    socket.join(req.session.id);
-    socket.on("incr", (cb) => {
-        req.session.reload((err) => {
-            if (err) {
-                return socket.disconnect();
-            }
-            req.session.count = (req.session.count || 0) + 1;
-            req.session.save(() => {
-                cb(req.session.count);
-            });
-        });
+    console.log(`connect ${socket.id}`);
+    socket.on("ping", (cb) => {
+        console.log("ping");
+        cb();
+    });
+    socket.on("disconnect", () => {
+        console.log(`disconnect ${socket.id}`);
     });
 });
-httpServer.listen(port, () => {
-    console.log(`application is running at: http://localhost:${port}`);
-});
+// import express from "express";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// import session from "express-session";
+// const port = process.env.PORT || 7071;
+// const app = express();
+// const httpServer = createServer(app);
+// const sessionMiddleware = session({
+//   secret: "changeit",
+//   resave: true,
+//   saveUninitialized: true,
+// });
+// app.use(sessionMiddleware);
+// app.get("/", function serveIndex (req: any, res: any) {
+//     res.write(`
+//       <html>
+//       <head>
+//       <title>微信小游戏</title>
+//       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+//       </head>
+//       <body>
+//       <header>
+//       <h1>《微信小游戏开发》</h1>
+//       </header>
+//       <ul>
+//         <li><a href="https://www.haozengrun.com">主页跳转</a></li>
+//         <li>游戏专用域名</li>
+//       </ul>
+//       <footer>ELEVEN</footer>
+//       </body>
+//       </html>
+//     `)
+//     res.end()
+// });
+// app.post("/incr", (req, res) => {
+//   const session: any = req.session;
+//   session.count = (session?.count || 0) + 1;
+//   res.status(200).end("" + session?.count);
+// });
+// app.post("/logout", (req, res) => {
+//   const sessionId = req.session.id;
+//   req.session.destroy(() => {
+//     io.to(sessionId).disconnectSockets();
+//     res.status(204).end();
+//   });
+// });
+// const io = new Server(httpServer, {
+//   allowRequest: (req: any, callback) => {
+//     const fakeRes = {
+//       getHeader() {
+//         return [];
+//       },
+//       setHeader(key: any, values: any) {
+//         req.cookieHolder = values[0];
+//       },
+//       writeHead() {},
+//     };
+//     // @ts-ignore
+//     sessionMiddleware(req, fakeRes, () => {
+//       if (req.session) {
+//         fakeRes.writeHead();
+//         req.session.save();
+//       }
+//       callback(null, true);
+//     });
+//   },
+// });
+// io.engine.on("initial_headers", (headers: { [x: string]: any; }, req: { cookieHolder: any; }) => {
+//   if (req.cookieHolder) {
+//     headers["set-cookie"] = req.cookieHolder;
+//     delete req.cookieHolder;
+//   }
+// });
+// io.on("connect", (socket) => {
+//   const req: any = socket.request;
+//   socket.join(req.session.id);
+//   socket.on("incr", (cb) => {
+//     req.session.reload((err: any) => {
+//       if (err) {
+//         return socket.disconnect();
+//       }
+//       req.session.count = (req.session.count || 0) + 1;
+//       req.session.save(() => {
+//         cb(req.session.count);
+//       });
+//     });
+//   });
+// });
+// httpServer.listen(port, () => {
+//   console.log(`application is running at: http://localhost:${port}`);
+// });
 // // import express, { Application, Request, Response, NextFunction } from "express";
 // // var http = require('http');
 // // const app: Application = express();
